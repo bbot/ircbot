96b6f23ba5e0a908e9b74a03fe937014bbf78263
bbot <bbot@magnesium.(none)>
2012-09-14 15:28:26 -0700

    added hsg.py

---
+++ hsg.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+"""
+hsg.py - Phenny module to find the HSG thread in the /co/ catalog.
+Written by Tatpurusa, ported to phenny by !!c1Q
+This is free and unencumbered software released into the public domain.
+"""
+import feedparser
+
+global cmod
+global hsglink
+
+def findhsg():
+    """fetches the rss, looks for some strings"""
+    global cmod
+    global hsglink
+
+    RSS_URL="http://catalog.neet.tv/co/feed.rss"
+
+    if 'cmod' in globals():
+        catalog = feedparser.parse(RSS_URL, modified=cmod)
+    else:
+        catalog = feedparser.parse(RSS_URL)
+    links = []
+    cmod=catalog.modified
+    #print catalog.status
+
+    for item in catalog["items"]:
+        if "homestuck general" in item["title"].lower():
+            links.append(item["link"])
+
+    for item in catalog["items"]:
+        if "hsg" in item["title"].lower():
+            links.append(item["link"])
+
+    for item in catalog["items"]:
+        if "homestuck general" in item["summary"].lower():
+            links.append(item["link"])
+
+    for item in catalog["items"]:
+        if "homestuck" in item["title"].lower():
+            links.append(item["link"])
+
+    for item in catalog["items"]:
+        if "homestuck" in item["summary"].lower():
+            links.append(item["link"])
+
+    if len(links) > 0:
+        hsglink=links[0]
+    else:
+        links.append(hsglink)
+
+    return links[0]
+
+def hsg(phenny, input):
+    phenny.say(findhsg())
+
+hsg.commands = ['hsg']
+hsg.priority = 'medium'
+
+if __name__ == '__main__':
+    print findhsg()
