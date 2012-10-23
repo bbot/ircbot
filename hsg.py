3e3f0cd41ff9d44f03f10de39729191fb27e35ae
bbot <github@bbot.org>
2012-10-23 17:13:17 -0400

    stopgap fix until I switch to the JSON version

--- hsg.py
+++ hsg.py
@@ -16,7 +16,7 @@ def findhsg():
     global cmod
     global hsglink
 
-    RSS_URL="http://boards.4chan.org/co/index.rss"
+    RSS_URL="http://catalog.neet.tv/co/feed.rss"
 
     if 'cmod' in globals():
         catalog = feedparser.parse(RSS_URL, modified=cmod)
