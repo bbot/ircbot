8acc30c71900fe813f8f6de7007a00c6470aae39
Moses Moore <moses-github@mozai.com>
2012-11-28 13:09:38 -0500

    Example of using phennyfake for testing

--- thanks.py
+++ thanks.py
@@ -1,18 +1,22 @@
 """
 Says 'no problem' when you say .thanks.
 
+Useful for sticking print() statements into it and reloading
+so you can do diagnostic or code introspection in a live bot.
+
 This is free and unencumbered software  released into the public domain.
 I wish our society wasn't so litigatious that I needed to say that.
 """
 
 def thanks(phenny, cmd_in):
   " Responds in the same context with 'No problem.' "
-  del(cmd_in) # unused
   phenny.say("No problem.")
+  del(cmd_in) # unused
 
 thanks.commands = ['thanks']
+# turns into r'.(thanks)(?: +(.*))'
 thanks.rule = ('$nick','(thanks|thank you)')
-# turns into r'^aradiabot[,:] +(thanks|thank you)'
+# turns into r'aradiabot[,:] +(thanks|thank you)'
 thanks.priority = 'low'
 
 if __name__ == '__main__':
