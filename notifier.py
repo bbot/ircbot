bc2def50593cf1dc0cb15532d002d0317bf9ca06
Samuel Bierwagen <github@bbot.org>
2012-12-06 18:30:52 -0800

    more banner fiddling

--- notifier.py
+++ notifier.py
@@ -35,9 +35,9 @@ SITES = {
   'update' : {
     'name': 'MSPA',
     'url' : 'http://mspaintadventures.com/rss/rss.xml',
-    'mesg': '===== ====== UPDATE UPDATE ====== =====\n' +
-            '==== http://mspaintadventures.com/ ====\n' +
-            '===== ====== UPDATE UPDATE ====== =====\n'
+    'mesg': '======= ======= UPDATE UPDATE ======= =======\n' +
+            '======= http://mspaintadventures.com/ =======\n' +
+            '======= ======= UPDATE UPDATE ======= =======\n'
   },
   'sbahj' : {
     'name': 'SBaHJ',
